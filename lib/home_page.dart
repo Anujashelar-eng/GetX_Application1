00134e6bfa24e891b68a62597f85ae7efd26451d
Anujashelar-eng <shelaranuja344@gmail.com>
2026-08-31 09:53:01 +0530

    My First Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(
+        title:Text("GetX Application"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body:Column(
+        ElevatedButton(
+          onPressed: (){
+
+          },
+          child: ,
+        )
+      )
+    );
+  }
+}
